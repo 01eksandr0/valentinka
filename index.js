15ca90f98bc4ace242929cc1036b65c92b779d8f
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2024-02-10 18:00:15 +0200

    1

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const btn1 = document.querySelector(".page-1-btn");
+const btn2 = document.querySelector(".page-2-btn");
+
 const page1 = document.querySelector(".page-1");
 const page2 = document.querySelector(".page-2");
+const page3 = document.querySelector(".page-3");
 
 const openPage2 = () => {
   page1.classList.add("is-opacity");
-  page2.classList.remove("is-opacity");
-  window.navigator.vibrate([
-    100, 30, 100, 30, 100, 200, 200, 30, 200, 30, 200, 200, 100, 30, 100, 30,
-    100,
-  ]);
+  setTimeout(() => page2.classList.remove("is-opacity"), 300);
 };
 btn1.addEventListener("click", openPage2);
+
+const openPage3 = () => {
+  page2.classList.add("is-opacity");
+  setTimeout(() => page3.classList.remove("is-opacity"), 300);
+};
+btn2.addEventListener("click", openPage3);
